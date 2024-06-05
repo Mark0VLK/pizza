e8f1549a1d093d89d6dba81b23f6dceb991dbdc9
Марк <markvl836@gmail.com>
2024-06-05 11:03:02 +0300

    Create database schema structure

--- src/main/java/com/example/pizza/model/PizzaOrder.java
+++ src/main/java/com/example/pizza/model/PizzaOrder.java
@@ -6,7 +6,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class Location {
+public class PizzaOrder {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
