8a35508512de73ec806f2da3ba414af3b1371d10
Ilya Kozlov <iluha339@gmail.com>
2024-06-05 22:39:19 +0300

    added users, location, category

--- src/main/java/com/example/pizza/model/Category.java
+++ src/main/java/com/example/pizza/model/Category.java
@@ -1,9 +1,11 @@
 package com.example.pizza.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+
+import java.security.Timestamp;
+import java.util.Collections;
+import java.util.Set;
 
 @Entity
 public class Category {
@@ -11,4 +13,20 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.PERSIST, fetch = FetchType.EAGER, orphanRemoval = true)
+    @JsonBackReference
+    private Set<Product> products = Collections.emptySet();
+
+    @Column(nullable = false)
+    private Timestamp created;
+
+    @Column(nullable = false)
+    private Timestamp changed;
+
+    @Column(name = "is_deleted", nullable = false, columnDefinition = "boolean default false")
+    private Boolean isDeleted = false;
 }
--- src/main/java/com/example/pizza/model/User.java
+++ src/main/java/com/example/pizza/model/User.java
@@ -1,14 +1,42 @@
 package com.example.pizza.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.security.Timestamp;
 
 @Entity
+@Table(name="users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    @Column(length = 50, nullable = false)
+    private String surname;
+
+    @Column(name = "phone_number", length = 20, nullable = false, unique = true)
+    private String phoneNumber;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 100, nullable = false)
+    private String password;
+
+    @Column(name = "birth_date")
+    private Timestamp birthDate;
+
+    @Column(nullable = false)
+    private Timestamp created;
+
+    @Column(nullable = false)
+    private Timestamp changed;
+
+    @Column(name = "is_deleted", nullable = false, columnDefinition = "boolean default false")
+    private Boolean isDeleted = false;
 }
+
