21c1baf8fde422077b7044c77a1459b67ae93e0f
Ilya Kozlov <iluha339@gmail.com>
2024-06-06 09:41:10 +0300

    bug fix

--- src/main/java/com/example/pizza/model/Dishes.java
+++ src/main/java/com/example/pizza/model/Dishes.java
@@ -1,31 +1,9 @@
 package com.example.pizza.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Index;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.PreRemove;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-import java.sql.Timestamp;
-import java.util.Collections;
-import java.util.Set;
 
 @Entity
 public class Dishes {
