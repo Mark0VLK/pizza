f2f3c2dac7e5947d127c2a665020ec0279ad4934
Egor Galyavetdinov <galyavet548@gmail.com>
2024-06-09 12:56:28 +0300

    Added all others repository

--- src/main/java/com/example/pizza/repositories/InformationRepository.java
+++ src/main/java/com/example/pizza/repositories/InformationRepository.java
@@ -3,5 +3,5 @@ package com.example.pizza.repositories;
 import com.example.pizza.model.Information;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface InformationRepository extends JpaRepository<Information, Integer> {
+public interface InformationRepository extends JpaRepository<Information, Long> {
 }
