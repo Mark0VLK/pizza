203153ece95caa9453faf1f996680aa67f1aa93f
Mark <markvl836@gmail.com>
2024-06-21 11:56:34 +0300

    The return of security

--- src/main/java/com/example/pizza/config/SecurityConfiguration.java
+++ src/main/java/com/example/pizza/config/SecurityConfiguration.java
@@ -42,57 +42,55 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
 
-                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
-//                        .requestMatchers(new AntPathRequestMatcher("/api/v1/auth/**")).permitAll()
-//
-//                        // CategoryController
-//                        .requestMatchers(GET, "/api/v1/category/**").hasAnyRole(ADMIN.name(), CUSTOMER.name())
-//                        .requestMatchers("/api/v1/category/**").hasRole(ADMIN.name())
-//                        .requestMatchers(POST, "/api/v1/category/**").hasAuthority(ADMIN_CREATE.name())
-//                        .requestMatchers(PUT, "/api/v1/category/**").hasAuthority(ADMIN_UPDATE.name())
-//                        .requestMatchers(DELETE, "/api/v1/category/**").hasAuthority(ADMIN_DELETE.name())
-//
-//                        // DishController
-//                        .requestMatchers(GET, "/api/v1/dish/**").hasAnyRole(ADMIN.name(), CUSTOMER.name())
-//                        .requestMatchers("/api/v1/dish/**").hasRole(ADMIN.name())
-//                        .requestMatchers(POST, "/api/v1/dish/**").hasAuthority(ADMIN_CREATE.name())
-//                        .requestMatchers(PUT, "/api/v1/dish/**").hasAuthority(ADMIN_UPDATE.name())
-//                        .requestMatchers(DELETE, "/api/v1/dish/**").hasAuthority(ADMIN_DELETE.name())
-//
-//                        // DrinkController
-//                        .requestMatchers(GET, "/api/v1/drink/**").hasAnyRole(ADMIN.name(), CUSTOMER.name())
-//                        .requestMatchers("/api/v1/drink/**").hasRole(ADMIN.name())
-//                        .requestMatchers(POST, "/api/v1/drink/**").hasAuthority(ADMIN_CREATE.name())
-//                        .requestMatchers(PUT, "/api/v1/drink/**").hasAuthority(ADMIN_UPDATE.name())
-//                        .requestMatchers(DELETE, "/api/v1/drink/**").hasAuthority(ADMIN_DELETE.name())
-//
-//                        // InformationController
-//                        .requestMatchers(GET, "/api/v1/information/**").hasAnyRole(ADMIN.name(), CUSTOMER.name())
-//                        .requestMatchers("/api/v1/information/**").hasRole(ADMIN.name())
-//                        .requestMatchers(POST, "/api/v1/information/**").hasAuthority(ADMIN_CREATE.name())
-//                        .requestMatchers(PUT, "/api/v1/information/**").hasAuthority(ADMIN_UPDATE.name())
-//                        .requestMatchers(DELETE, "/api/v1/information/**").hasAuthority(ADMIN_DELETE.name())
-//
-//                        // LocationController
-//                        .requestMatchers(GET, "/api/v1/location/**").hasAnyRole(ADMIN.name(), CUSTOMER.name())
-//                        .requestMatchers("/api/v1/location/**").hasRole(ADMIN.name())
-//                        .requestMatchers(POST, "/api/v1/location/**").hasAuthority(ADMIN_CREATE.name())
-//                        .requestMatchers(PUT, "/api/v1/location/**").hasAuthority(ADMIN_UPDATE.name())
-//                        .requestMatchers(DELETE, "/api/v1/location/**").hasAuthority(ADMIN_DELETE.name())
-//
-//                        // OrderController
-//                        .requestMatchers("/api/v1/order/**").hasAnyRole(ADMIN.name(), CUSTOMER.name())
-//
-//                        // PizzaController
-//                        .requestMatchers(GET, "/api/v1/pizza/**").hasAnyRole(ADMIN.name(), CUSTOMER.name())
-//                        .requestMatchers("/api/v1/pizza/**").hasRole(ADMIN.name())
-//                        .requestMatchers(POST, "/api/v1/pizza/**").hasAuthority(ADMIN_CREATE.name())
-//                        .requestMatchers(PUT, "/api/v1/pizza/**").hasAuthority(ADMIN_UPDATE.name())
-//                        .requestMatchers(DELETE, "/api/v1/pizza/**").hasAuthority(ADMIN_DELETE.name())
-//
-//                        // UserController
-//                        .requestMatchers("/api/v1/user/**").hasRole(ADMIN.name())
+                        .requestMatchers(new AntPathRequestMatcher("/api/v1/auth/**")).permitAll()
 
+                        // CategoryController
+                        .requestMatchers(GET, "/api/v1/category/**").hasAnyRole(ADMIN.name(), CUSTOMER.name())
+                        .requestMatchers("/api/v1/category/**").hasRole(ADMIN.name())
+                        .requestMatchers(POST, "/api/v1/category/**").hasAuthority(ADMIN_CREATE.name())
+                        .requestMatchers(PUT, "/api/v1/category/**").hasAuthority(ADMIN_UPDATE.name())
+                        .requestMatchers(DELETE, "/api/v1/category/**").hasAuthority(ADMIN_DELETE.name())
+
+                        // DishController
+                        .requestMatchers(GET, "/api/v1/dish/**").hasAnyRole(ADMIN.name(), CUSTOMER.name())
+                        .requestMatchers("/api/v1/dish/**").hasRole(ADMIN.name())
+                        .requestMatchers(POST, "/api/v1/dish/**").hasAuthority(ADMIN_CREATE.name())
+                        .requestMatchers(PUT, "/api/v1/dish/**").hasAuthority(ADMIN_UPDATE.name())
+                        .requestMatchers(DELETE, "/api/v1/dish/**").hasAuthority(ADMIN_DELETE.name())
+
+                        // DrinkController
+                        .requestMatchers(GET, "/api/v1/drink/**").hasAnyRole(ADMIN.name(), CUSTOMER.name())
+                        .requestMatchers("/api/v1/drink/**").hasRole(ADMIN.name())
+                        .requestMatchers(POST, "/api/v1/drink/**").hasAuthority(ADMIN_CREATE.name())
+                        .requestMatchers(PUT, "/api/v1/drink/**").hasAuthority(ADMIN_UPDATE.name())
+                        .requestMatchers(DELETE, "/api/v1/drink/**").hasAuthority(ADMIN_DELETE.name())
+
+                        // InformationController
+                        .requestMatchers(GET, "/api/v1/information/**").hasAnyRole(ADMIN.name(), CUSTOMER.name())
+                        .requestMatchers("/api/v1/information/**").hasRole(ADMIN.name())
+                        .requestMatchers(POST, "/api/v1/information/**").hasAuthority(ADMIN_CREATE.name())
+                        .requestMatchers(PUT, "/api/v1/information/**").hasAuthority(ADMIN_UPDATE.name())
+                        .requestMatchers(DELETE, "/api/v1/information/**").hasAuthority(ADMIN_DELETE.name())
+
+                        // LocationController
+                        .requestMatchers(GET, "/api/v1/location/**").hasAnyRole(ADMIN.name(), CUSTOMER.name())
+                        .requestMatchers("/api/v1/location/**").hasRole(ADMIN.name())
+                        .requestMatchers(POST, "/api/v1/location/**").hasAuthority(ADMIN_CREATE.name())
+                        .requestMatchers(PUT, "/api/v1/location/**").hasAuthority(ADMIN_UPDATE.name())
+                        .requestMatchers(DELETE, "/api/v1/location/**").hasAuthority(ADMIN_DELETE.name())
+
+                        // OrderController
+                        .requestMatchers("/api/v1/order/**").hasAnyRole(ADMIN.name(), CUSTOMER.name())
+
+                        // PizzaController
+                        .requestMatchers(GET, "/api/v1/pizza/**").hasAnyRole(ADMIN.name(), CUSTOMER.name())
+                        .requestMatchers("/api/v1/pizza/**").hasRole(ADMIN.name())
+                        .requestMatchers(POST, "/api/v1/pizza/**").hasAuthority(ADMIN_CREATE.name())
+                        .requestMatchers(PUT, "/api/v1/pizza/**").hasAuthority(ADMIN_UPDATE.name())
+                        .requestMatchers(DELETE, "/api/v1/pizza/**").hasAuthority(ADMIN_DELETE.name())
+
+                        // UserController
+                        .requestMatchers("/api/v1/user/**").hasRole(ADMIN.name())
 
                         .anyRequest().authenticated()
                 )
